894614b60c1d4a5b82516af113a668f9dd8d9b43
Kusuma Hendra <me@kusumahendra.com>
2017-11-30 14:30:21 +0800

    + loading feedback + warning state to callout

--- resources/views/frontend/form/index.blade.php
+++ resources/views/frontend/form/index.blade.php
@@ -11,8 +11,8 @@ Indonesia Holiday Checker
         <div class="grid-x grid-y">
             <div class="small-12 medium-6 large-4">
                 <h1>Indonesian Holiday Checker</h1>
-                <input type="text" class="datepicker-input" name="date" placeholder="Insert date here">
-                <button id="check-date" type="submit" class="pull-right button primary">Submit</button>
+                <input type="text" class="datepicker-input" name="date" placeholder="Insert date here" readonly>
+                <button id="check-date" type="submit" class="pull-right button primary">Submit <i id="loading-feedback" class="hide fa fa-circle-o-notch fa-spin"></i></button>
                 <div class="clearfix"></div>
                 <div class="callout hide" id="result"></div>
             </div>
@@ -34,24 +34,27 @@ Indonesia Holiday Checker
             format: 'yyyy-mm-dd'
         });
         $('#check-date').click(function(){
+            $('#loading-feedback').removeClass('hide');
             date = $('input[name=date]').val();
             $('#result').html('');
-            $('#result').addClass('hide');
+            $('#result').addClass('hide').removeClass('warning');
             $.ajax({
                 type: "GET",
                 url: "{{ url("/api") }}/check/"+date,
                 error: function(xhr) {
+                    $('#loading-feedback').addClass('hide');
                     $('#result').html('<p>'+xhr.responseJSON.message+'</p>');
                     console.log(xhr.responseJSON.message);
-                    $('#result').removeClass('hide');
+                    $('#result').removeClass('hide').addClass('warning');
                 },
                 success: function(data,xhr){
+                    $('#loading-feedback').addClass('hide');
                     $('#result').append(data.date_formated+' is ');
                     if (data.name){
                         $('#result').append(data.name);
                     }
                     else{
-                        $('#result').append('not an Indonesia Holiday');
+                        $('#result').append('not an Indonesian holiday');
                     }
                     console.log(data);
                     $('#result').removeClass('hide');
